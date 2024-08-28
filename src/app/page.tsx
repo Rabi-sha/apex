0da24840243dae36ca250acbf4413f6c2eb88af2
Rabi-sha <rabiaw199@gmail.com>
2024-08-28 20:08:27 +0500

    change correctin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <div className="pt-6 space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-          “Tailwind CSS is the only framework that I've seen scale
+          “Tailwind CSS is the only framework that I &apos; ve seen scale
           on large teams. It’s easy to customize, adapts to any design,
           and the build size is tiny.”
         </p>
